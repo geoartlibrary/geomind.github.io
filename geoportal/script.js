eec669c824e455ccfb6a70218e8517d5d22cab5c
GeologiaSLG <a.leicaperez@ugto.mx>
2026-04-14 17:14:07 -0500

    GEOLGIA

--- geoportal/script.js
+++ geoportal/script.js
@@ -44,14 +44,50 @@ document.addEventListener("DOMContentLoaded", () => {
     "https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?",
     { layers: 'MODIS_Terra_CorrectedReflectance_TrueColor', format: 'image/png', transparent: true, opacity: 0.8 }
   );
-  const temperatura = L.tileLayer.wms(
-    "https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?",
-    { layers: 'MODIS_Terra_Land_Surface_Temp_Day', format: 'image/png', transparent: true, opacity: 0.8 }
-  );
-
-  L.control.zoom({ position: 'topright' }).addTo(map);
-  L.control.scale({ position: 'bottomleft', imperial: false, maxWidth: 200 }).addTo(map);
-
+ const NASAGIBS_ModisTerraTrueColorCR = L.tileLayer(
+      'https://map1.vis.earthdata.nasa.gov/wmts-webmerc/MODIS_Terra_CorrectedReflectance_TrueColor/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}',
+      {
+        attribution: 'Imagery provided by NASA GIBS (ESDIS)',
+        bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+        minZoom: 1,
+        maxZoom: 9,
+        format: 'jpg',
+        time: '',
+        tilematrixset: 'GoogleMapsCompatible_Level'
+      }
+    );
+const NASAGIBS_ModisTerraBands367CR = L.tileLayer(
+      'https://map1.vis.earthdata.nasa.gov/wmts-webmerc/MODIS_Terra_CorrectedReflectance_Bands367/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}',
+      {
+        attribution: 'Imagery provided by NASA GIBS (ESDIS)',
+        bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+        minZoom: 1,
+        maxZoom: 9,
+        format: 'jpg',
+        time: '',
+        tilematrixset: 'GoogleMapsCompatible_Level'
+      }
+    );
+    const geologyLayer = L.tileLayer.wms(
+      "https://mapsref.brgm.fr/wxs/1GG/CGMW_Bedrock_and_Structural_Geology?",
+      {
+        layers: 'World_CGMW_50M_Geology', // nombre de la capa según GetCapabilities
+        format: 'image/png',
+        transparent: true,   // importante para ver la base debajo
+        version: '1.3.0',
+        attribution: 'BRGM · CGMW Bedrock & Structural Geology'
+      }
+    );
+    const geologysudamerica = L.tileLayer.wms(
+      "https://srvags.sgc.gov.co/arcgis/services/Mapa_Geologico_Sur_America/GeologicalMapSouthAmerican/MapServer/WmsServer?",
+      {
+        layers: '8', // nombre de la capa según GetCapabilities
+        format: 'image/png',
+        transparent: true,
+        version: '1.3.0',
+        attribution: 'SGC · Mapa Geológico de Suramérica'
+      }
+    )
   // Barra de coordenadas UTM
   const coordDiv = L.control({ position: 'bottomleft' });
   coordDiv.onAdd = function () {
@@ -78,13 +114,17 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   document.getElementById("toggle-hidrologia").addEventListener("change", e => {
-    e.target.checked ? map.addLayer(biota) : map.removeLayer(biota);
+    e.target.checked ? map.addLayer(NASAGIBS_ModisTerraBands367CR) : map.removeLayer(NASAGIBS_ModisTerraBands367CR);
   });
-  document.getElementById("toggle-temperatura").addEventListener("change", e => {
-    e.target.checked ? map.addLayer(temperatura) : map.removeLayer(temperatura);
+document.getElementById("toggle-Lansat").addEventListener("change", e => {
+    e.target.checked ? map.addLayer(NASAGIBS_ModisTerraTrueColorCR) : map.removeLayer(NASAGIBS_ModisTerraTrueColorCR);
   });
-
-  // ═══════════════════════════════════════════════
+document.getElementById("toggle-geologia").addEventListener("change", e => {
+    e.target.checked ? map.addLayer(geologyLayer) : map.removeLayer(geologyLayer);
+  });
+document.getElementById("toggle-geologiasub").addEventListener("change", e => {
+    e.target.checked ? map.addLayer(geologysudamerica) : map.removeLayer(geologysudamerica);
+  });
   //  HERRAMIENTA DE CAPTURA DE COORDENADAS
   // ═══════════════════════════════════════════════
 
